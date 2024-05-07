db446b4471eeca9a212db825b7d73102dd9ca667
Macbook <macbook@Sher-Ali.local>
2024-05-07 14:03:41 +0500

    fixes

--- src/app/_utils/common/constants.js
+++ src/app/_utils/common/constants.js
@@ -1,2 +1,3 @@
 //export const Listener_URL = "http://localhost:3001";
-export const Listener_URL = "https://chat.mqasim.com/";
+// export const Listener_URL = "https://chat.mqasim.com/";
+export const Listener_URL = "https://theplaylist.rollingvideogamesbooking.com/";
