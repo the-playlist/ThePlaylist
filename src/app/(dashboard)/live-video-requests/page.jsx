7772b85744e3c99633c46d28836f948c4812889d
Macbook <macbook@Sher-Ali.local>
2024-05-22 17:39:19 +0500

    latest fixes

--- src/app/(dashboard)/live-video-requests/page.jsx
+++ src/app/(dashboard)/live-video-requests/page.jsx
@@ -107,59 +107,65 @@ const StreamResponse = () => {
             </div>
           )}
           {streamContent?.length > 0 ? (
-            <div className="flex flex-wrap items-center justify-start ">
-              {streamContent.map((item, index) => {
-                return (
-                  <div className="card  w-[32%] bg-base-100 shadow-xl mr-4  mb-4 p-5">
-                    <div className="flex justify-between items-center mb-3 ">
-                      <h2 className="card-title">Table no:{item?.tableNo} </h2>
-                    </div>
-                    <div className="bg-black h-56 rounded-md">
-                      <figure>
-                        <StreamRequest item={item} socket={socket} />
-                      </figure>
-                    </div>
-                    <div className="mt-3">
-                      <div className=" flex justify-between items-center">
-                        <div className="card-actions justify-end w-full mr-2">
-                          <button
-                            onClick={() => {
-                              let payload = {
-                                callId: item?.callId,
-                                id: item?._id,
-                                isAccepted: true,
-                                isActive: true,
-                              };
+            <>
+              <span className=" text-lg font-semibold mb-4">Requests</span>
 
-                              changeStatusHandler(payload);
-                            }}
-                            className="btn btn-primary bg-primary border-0 hover:bg-primary  text-black w-full"
-                          >
-                            Accept
-                          </button>
-                        </div>
-                        <div className="card-actions justify-end w-full  ml-2">
-                          <button
-                            onClick={() => {
-                              let payload = {
-                                callId: item?.callId,
-                                id: item?._id,
-                                isActive: false,
-                              };
+              <div className="flex flex-wrap items-center justify-start ">
+                {streamContent.map((item, index) => {
+                  return (
+                    <div className="card  w-[32%] bg-base-100 shadow-xl mr-4  mb-4 p-5">
+                      <div className="flex justify-between items-center mb-3 ">
+                        <h2 className="card-title">
+                          Table no:{item?.tableNo}{" "}
+                        </h2>
+                      </div>
+                      <div className="bg-black h-56 rounded-md">
+                        <figure>
+                          <StreamRequest item={item} socket={socket} />
+                        </figure>
+                      </div>
+                      <div className="mt-3">
+                        <div className=" flex justify-between items-center">
+                          <div className="card-actions justify-end w-full mr-2">
+                            <button
+                              onClick={() => {
+                                let payload = {
+                                  callId: item?.callId,
+                                  id: item?._id,
+                                  isAccepted: true,
+                                  isActive: true,
+                                };
 
-                              changeStatusHandler(payload);
-                            }}
-                            className="btn btn-primary bg-black border-0 text-white hover:bg-black w-full"
-                          >
-                            Reject
-                          </button>
+                                changeStatusHandler(payload);
+                              }}
+                              className="btn btn-primary bg-primary border-0 hover:bg-primary  text-black w-full"
+                            >
+                              Accept
+                            </button>
+                          </div>
+                          <div className="card-actions justify-end w-full  ml-2">
+                            <button
+                              onClick={() => {
+                                let payload = {
+                                  callId: item?.callId,
+                                  id: item?._id,
+                                  isActive: false,
+                                };
+
+                                changeStatusHandler(payload);
+                              }}
+                              className="btn btn-primary bg-black border-0 text-white hover:bg-black w-full"
+                            >
+                              Reject
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
+                  );
+                })}
+              </div>
+            </>
           ) : (
             <div className="text-black text-lg flex h-[90vh] items-center justify-center font-semibold">
               {"No stream requests yet"}
