f91566ae0a325d95dcfebd47586cf0636e8971a0
Qasim Subhani <qasim.subhani@devexcelit.com>
2024-06-21 18:29:37 +0500

    main logic

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // env: {
-  //   NEXT_PUBLIC_SOCKET_URL: "https://chat.mqasim.com/",
-  // },
   reactStrictMode: false,
 };
 module.exports = nextConfig;
