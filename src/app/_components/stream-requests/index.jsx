f65f25192c76954d6372a361817e23d3fcbb9dd3
Qasim Subhani <qasim.subhani@devexcelit.com>
2024-07-04 15:49:10 +0500

    auto request cancellation

--- src/app/_components/stream-requests/index.jsx
+++ src/app/_components/stream-requests/index.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { memo, useEffect, useMemo } from "react";
+import React, { memo, useEffect, useMemo, useState } from "react";
 import {
   LivestreamLayout,
   StreamCall,
@@ -11,70 +11,84 @@ import {
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./style.css";
 
-const StreamRequests = memo(({ item, fullScreen, isAccepted }) => {
-  const { callId, token, userId } = item;
-  const apiKey = "d7r2k5cjtzqj";
+const StreamRequests = memo(
+  ({ item, fullScreen, isAccepted, userLeftHandler }) => {
+    const { callId, token, userId } = item;
+    const apiKey = "d7r2k5cjtzqj";
 
-  // Create user object only once
-  const user = useMemo(
-    () => ({
-      id: userId,
-      name: "Stream-Viewer",
-      image: "https://getstream.io/random_svg/?id=oliver&name=Stream-Viewer",
-    }),
-    [userId]
-  );
+    // Create user object only once
+    const user = useMemo(
+      () => ({
+        id: userId,
+        name: "Stream-Viewer",
+        image: "https://getstream.io/random_svg/?id=oliver&name=Stream-Viewer",
+      }),
+      [userId]
+    );
 
-  // Initialize StreamVideoClient and call only once
-  const { client, call } = useMemo(() => {
-    const client = new StreamVideoClient({ apiKey, user, token });
-    const call = client.call("livestream", callId);
-    return { client, call };
-  }, [apiKey, user, token, callId]);
+    // Initialize StreamVideoClient and call only once
+    const { client, call } = useMemo(() => {
+      const client = new StreamVideoClient({ apiKey, user, token });
+      const call = client.call("livestream", callId);
+      return { client, call };
+    }, [apiKey, user, token, callId]);
 
-  useEffect(() => {
-    // Join call and disable camera/microphone only once
-    call.join();
-    call.camera.disable();
-    call.microphone.disable();
-  }, [call]);
+    useEffect(() => {
+      const handleCallEnded = (e, eventType) => {
+        userLeftHandler();
+      };
+      // Add listener for call ended event
+      call.on("call.session_participant_left", handleCallEnded);
+      // Cleanup function to remove listener on unmount
+      return () => call.off("call.session_participant_left", handleCallEnded);
+    }, [call]);
 
-  return (
-    <div>
-      <StreamVideo client={client}>
-        <StreamCall call={call}>
-          {fullScreen ? (
-            <div className="fullScreenContainer">
-              <div className="innerContainer">
-                <StreamTheme>
+    useEffect(() => {
+      // Join call and disable camera/microphone only once
+      call.join();
+      call.camera.disable();
+      call.microphone.disable();
+    }, [call]);
+
+    return (
+      <div>
+        <StreamVideo client={client}>
+          <StreamCall call={call}>
+            {fullScreen ? (
+              <div className="fullScreenContainer">
+                <div className="innerContainer">
+                  <StreamTheme>
+                    <LivestreamLayout
+                      showParticipantCount={false}
+                      showDuration={true}
+                      showLiveBadge={true}
+                      muted={false}
+                    />
+                  </StreamTheme>
+                </div>
+              </div>
+            ) : (
+              <div
+                className={
+                  isAccepted ? "acceptedContainer" : "defaultContainer"
+                }
+              >
+                <div className="innerContainer">
                   <LivestreamLayout
                     enableFullscreen={true}
                     showParticipantCount={false}
-                    showDuration={true}
-                    showLiveBadge={true}
+                    showDuration={false}
+                    showLiveBadge={false}
                     muted={false}
                   />
-                </StreamTheme>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div
-              className={isAccepted ? "acceptedContainer" : "defaultContainer"}
-            >
-              <div className="innerContainer">
-                <LivestreamLayout
-                  showParticipantCount={false}
-                  showDuration={false}
-                  showLiveBadge={false}
-                  muted={false}
-                />
-              </div>
-            </div>
-          )}
-        </StreamCall>
-      </StreamVideo>
-    </div>
-  );
-});
+            )}
+          </StreamCall>
+        </StreamVideo>
+      </div>
+    );
+  }
+);
 
 export default StreamRequests;
