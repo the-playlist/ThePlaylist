f62509825beb94b9c1820e1654a7b641a9452e23
Qasim Subhani <qasimsubhani786@gmail.com>
2024-08-23 11:57:18 +0500

    the playlist PWa changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 const nextConfig = {
   reactStrictMode: false,
 };
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
