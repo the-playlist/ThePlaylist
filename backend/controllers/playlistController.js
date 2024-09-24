55cf696ff277a6ee4d16ffbe23f4e09011e28d2b
sher-ali <she.ali@devexcelit.com>
2024-09-25 01:45:06 +0500

    fixes

--- backend/controllers/playlistController.js
+++ backend/controllers/playlistController.js
@@ -401,8 +401,8 @@ export const deleteSongFromPlaylistByIdV2 = async (req, res, next) => {
     { isDeleted: isDeleted, isFixed: false },
     { new: true }
   );
-  const activeSongs = await PlaylistV2.find({ isDeleted: false });
-  if (activeSongs?.length === 1) {
+  const playlist = await PlaylistV2.aggregate(songFromPlaylistV2);
+  if (playlist?.length === 1) {
     await PlaylistType.updateOne(
       {
         _id: SETTING_ID,
@@ -412,7 +412,38 @@ export const deleteSongFromPlaylistByIdV2 = async (req, res, next) => {
       }
     );
   }
-  const updatedSongs = activeSongs.map((song, index) => ({
+  const flattenPlaylist = (playlist) =>
+    playlist.map((item) => {
+      const duration = convertTimeToSeconds(item?.songData?.songDuration);
+      const introSec = item?.songData?.introSec || 0;
+      const totalDuration = formatTime(duration + parseInt(introSec));
+
+      return {
+        _id: item._id,
+        playerName: `${item?.assignedPlayer?.firstName} ${item?.assignedPlayer?.lastName}`,
+        assignedPlayerId: item.assignedPlayer?._id,
+        qualifiedPlayers: item?.qualifiedPlayers,
+        songId: item.songData._id,
+        title: item.songData.title,
+        artist: item.songData.artist,
+        introSec,
+        songDuration: totalDuration,
+        isFav: item.songData.isFav,
+        dutyStatus: item?.assignedPlayer?.duty?.status,
+        category: item.songData.category,
+        tableUpVote: item.upVote,
+        tableDownVote: item.downVote,
+        upVote: item.upVoteCount,
+        downVote: item.downVoteCount,
+        sortOrder: item.sortOrder,
+        sortByMaster: item?.sortByMaster,
+        addByCustomer: item.addByCustomer,
+        isFixed: item?.isFixed,
+      };
+    });
+
+  let flattenedPlaylist = flattenPlaylist(playlist);
+  const updatedSongs = flattenedPlaylist.map((song, index) => ({
     ...song,
     sortOrder: index,
   }));
