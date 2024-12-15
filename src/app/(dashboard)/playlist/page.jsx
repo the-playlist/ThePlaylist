d7ed9dbb95d974a4cd8aa46ba498f931e1cd8687
sher-ali <sher.ali@devexcelit.com>
2024-12-16 00:53:04 +0500

    flag

--- src/app/(dashboard)/playlist/page.jsx
+++ src/app/(dashboard)/playlist/page.jsx
@@ -331,150 +331,151 @@ const page = () => {
     }
   }, [fixedContent]);
 
-  // const fetchPlaylistSongList = async (firstFetch) => {
-  //   try {
-  //     let shouldNotResetTime = JSON.parse(
-  //       localStorage.getItem("isRequestInProgress") || "false"
-  //     );
-  //     console.log("==>", shouldNotResetTime);
-
-  //     let response = await getPlaylistSongListApi();
-  //     const newConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-  //       autoConnect: false,
-  //     });
-  //     newConnection.connect();
-  //     if (response && !response?.isError) {
-  //       const { isFavortiteListType, isFixedItems, isNotFixed, completeList } =
-  //         response?.data?.content;
-
-  //       const playlistWithId = isNotFixed?.map((item, index) => ({
-  //         ...item,
-  //         id: index, // Add a unique id if it doesn't exist
-  //       }));
-  //       setCompleteList(completeList);
-
-  //       if (completeList?.length != 0 && completeList?.length < 30) {
-  //         fetchSongsList(completeList);
-  //       }
-  //       if (completeList?.length > 0) {
-  //         setIsFavExist(completeList?.filter((item) => item?.isFav));
-  //       }
-
-  //       if (
-  //         isFixedItems?.length > 0 &&
-  //         currentSong?.title == "" &&
-  //         currentSongSecond == 0 &&
-  //         !shouldNotResetTime
-  //       ) {
-  //         const { playerName, title, _id } = isFixedItems[0];
-
-  //         dispatch(
-  //           setCurrentSong({
-  //             title: title,
-  //             player: playerName,
-  //             id: _id,
-  //             duration: convertTimeToSeconds(isFixedItems[0].songDuration),
-  //           })
-  //         );
-  //         dispatch(
-  //           setCurrentSongSecond(
-  //             convertTimeToSeconds(isFixedItems[0].songDuration)
-  //           )
-  //         );
-  //       }
-  //       setFixedContent([...isFixedItems] || []);
-  //       setNonFixedContent([...playlistWithId] || []);
-  //       setIsFavSongs(isFavortiteListType);
-  //       dispatch(setPlaylistLength(isFixedItems?.length));
-
-  //       newConnection.emit("insertSongIntoPlaylistRequest-v2", {
-  //         playlist: completeList,
-  //         isInsert: false,
-  //       });
-  //     }
-  //     setIsAdvanceButtonDisable(false);
-
-  //     setIsLoading(false);
-  //   } catch (error) {
-  //     console.error("Fetch failed:", error);
-  //   }
-  // };
-  let fetchQueue = Promise.resolve();
-
   const fetchPlaylistSongList = async (firstFetch) => {
-    fetchQueue = fetchQueue.then(async () => {
-      try {
-        let response = await getPlaylistSongListApi();
-        const newConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-          autoConnect: false,
-        });
-        newConnection.connect();
-        if (response && !response?.isError) {
-          const {
-            isFavortiteListType,
-            isFixedItems,
-            isNotFixed,
-            completeList,
-          } = response?.data?.content;
+    try {
+      let shouldNotResetTime = JSON.parse(
+        localStorage.getItem("isRequestInProgress") || "false"
+      );
+      console.log("==>", shouldNotResetTime);
 
-          const playlistWithId = isNotFixed?.map((item, index) => ({
-            ...item,
-            id: index,
-          }));
+      let response = await getPlaylistSongListApi();
+      const newConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+        autoConnect: false,
+      });
+      newConnection.connect();
+      if (response && !response?.isError) {
+        const { isFavortiteListType, isFixedItems, isNotFixed, completeList } =
+          response?.data?.content;
 
-          setCompleteList(completeList);
+        const playlistWithId = isNotFixed?.map((item, index) => ({
+          ...item,
+          id: index, // Add a unique id if it doesn't exist
+        }));
+        setCompleteList(completeList);
 
-          if (completeList?.length != 0 && completeList?.length < 30) {
-            fetchSongsList(completeList);
-          }
-          if (completeList?.length > 0) {
-            setIsFavExist(completeList?.filter((item) => item?.isFav));
-          }
-
-          if (
-            isFixedItems?.length > 0 &&
-            (!currentSong?.title ||
-              currentSong?.title === "" ||
-              currentSongSecond === 0)
-          ) {
-            const { playerName, title, _id } = isFixedItems[0];
-            dispatch(
-              setCurrentSong({
-                title: title,
-                player: playerName,
-                id: _id,
-                duration: convertTimeToSeconds(isFixedItems[0].songDuration),
-              })
-            );
-            dispatch(
-              setCurrentSongSecond(
-                convertTimeToSeconds(isFixedItems[0].songDuration)
-              )
-            );
-          }
-
-          setFixedContent([...isFixedItems] || []);
-          setNonFixedContent([...playlistWithId] || []);
-          setIsFavSongs(isFavortiteListType);
-          dispatch(setPlaylistLength(isFixedItems?.length));
-
-          newConnection.emit("insertSongIntoPlaylistRequest-v2", {
-            playlist: completeList,
-            isInsert: false,
-          });
+        if (completeList?.length != 0 && completeList?.length < 30) {
+          fetchSongsList(completeList);
         }
-        setIsAdvanceButtonDisable(false);
+        if (completeList?.length > 0) {
+          setIsFavExist(completeList?.filter((item) => item?.isFav));
+        }
 
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
+        if (
+          isFixedItems?.length > 0 &&
+          currentSong?.title == "" &&
+          currentSongSecond == 0 &&
+          !shouldNotResetTime
+        ) {
+          const { playerName, title, _id } = isFixedItems[0];
 
-        console.error("Fetch failed:", error);
-      } finally {
-        setIsLoading(false);
+          dispatch(
+            setCurrentSong({
+              title: title,
+              player: playerName,
+              id: _id,
+              duration: convertTimeToSeconds(isFixedItems[0].songDuration),
+            })
+          );
+          dispatch(
+            setCurrentSongSecond(
+              convertTimeToSeconds(isFixedItems[0].songDuration)
+            )
+          );
+        }
+        setFixedContent([...isFixedItems] || []);
+        setNonFixedContent([...playlistWithId] || []);
+        setIsFavSongs(isFavortiteListType);
+        dispatch(setPlaylistLength(isFixedItems?.length));
+
+        newConnection.emit("insertSongIntoPlaylistRequest-v2", {
+          playlist: completeList,
+          isInsert: false,
+        });
       }
-    });
+      setIsAdvanceButtonDisable(false);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Fetch failed:", error);
+    }
   };
+
+  // let fetchQueue = Promise.resolve();
+
+  // const fetchPlaylistSongList = async (firstFetch) => {
+  //   fetchQueue = fetchQueue.then(async () => {
+  //     try {
+  //       let response = await getPlaylistSongListApi();
+  //       const newConnection = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+  //         autoConnect: false,
+  //       });
+  //       newConnection.connect();
+  //       if (response && !response?.isError) {
+  //         const {
+  //           isFavortiteListType,
+  //           isFixedItems,
+  //           isNotFixed,
+  //           completeList,
+  //         } = response?.data?.content;
+
+  //         const playlistWithId = isNotFixed?.map((item, index) => ({
+  //           ...item,
+  //           id: index,
+  //         }));
+
+  //         setCompleteList(completeList);
+
+  //         if (completeList?.length != 0 && completeList?.length < 30) {
+  //           fetchSongsList(completeList);
+  //         }
+  //         if (completeList?.length > 0) {
+  //           setIsFavExist(completeList?.filter((item) => item?.isFav));
+  //         }
+
+  //         if (
+  //           isFixedItems?.length > 0 &&
+  //           (!currentSong?.title ||
+  //             currentSong?.title === "" ||
+  //             currentSongSecond === 0)
+  //         ) {
+  //           const { playerName, title, _id } = isFixedItems[0];
+  //           dispatch(
+  //             setCurrentSong({
+  //               title: title,
+  //               player: playerName,
+  //               id: _id,
+  //               duration: convertTimeToSeconds(isFixedItems[0].songDuration),
+  //             })
+  //           );
+  //           dispatch(
+  //             setCurrentSongSecond(
+  //               convertTimeToSeconds(isFixedItems[0].songDuration)
+  //             )
+  //           );
+  //         }
+
+  //         setFixedContent([...isFixedItems] || []);
+  //         setNonFixedContent([...playlistWithId] || []);
+  //         setIsFavSongs(isFavortiteListType);
+  //         dispatch(setPlaylistLength(isFixedItems?.length));
+
+  //         newConnection.emit("insertSongIntoPlaylistRequest-v2", {
+  //           playlist: completeList,
+  //           isInsert: false,
+  //         });
+  //       }
+  //       setIsAdvanceButtonDisable(false);
+
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       setIsLoading(false);
+
+  //       console.error("Fetch failed:", error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // };
 
   const deleteSongFromPlaylistHandler = async (id, isTrashPress, hideSong) => {
     setIsAdvanceButtonDisable(true);
