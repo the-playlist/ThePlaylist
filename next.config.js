8fc89f024f72b979474114cf9b131819984810f2
Sher Ali <sher.ali@devexcelit.com>
2024-12-30 16:22:53 +0500

    reverting config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  cacheOnFrontEndNav: false,
-  aggressiveFrontEndNavCaching: false,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: true,
+  disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
   },
